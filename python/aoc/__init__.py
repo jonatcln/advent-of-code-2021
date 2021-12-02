ca626639e782f63568827a22234225ac77ab8b2d
jonatcln <41806047+jonatcln@users.noreply.github.com>
2021-12-02 06:24:37 +0100

    prepare(py): add day03 project files

--- python/aoc/__init__.py
+++ python/aoc/__init__.py
@@ -1,8 +1,9 @@
-from . import day01, day02
+from . import day01, day02, day03
 
 YEAR = 2021
 
 AOC = {
     1: {1: day01.part1, 2: day01.part2},
     2: {1: day02.part1, 2: day02.part2},
+    3: {1: day03.part1, 2: day03.part2},
 }
