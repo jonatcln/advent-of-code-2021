a94c4e8aac307058efacfca4abbc62856341be46
jonatcln <41806047+jonatcln@users.noreply.github.com>
2021-12-11 12:44:19 +0100

    feat(py/utils): extend Point and Grid api

--- python/aoc2021/utils.py
+++ python/aoc2021/utils.py
@@ -30,6 +30,28 @@ class Point:
     def as_geometric_point(self) -> GeometricPoint:
         return GeometricPoint(float(self.x), float(self.y))
 
+    def diagonally_adjacents(self) -> Iterator['Point']:
+        for dy in (-1, 1):
+            for dx in (-1, 1):
+                yield Point(self.x + dx, self.y + dy)
+
+    def adjacents(self) -> Iterator['Point']:
+        return (
+            Point(self.x, self.y - 1),
+            Point(self.x - 1, self.y),
+            Point(self.x + 1, self.y),
+            Point(self.x, self.y + 1),
+        ).iter()
+
+    def adjacents_with_diagonals(self) -> Iterator['Point']:
+        for dy in (-1, 0, 1):
+            for dx in (-1, 0, 1):
+                if dx != 0 or dy != 0:
+                    yield Point(self.x + dx, self.y + dy)
+
+    def __repr__(self):
+        return str((self.x, self.y))
+
 
 class Grid(Generic[T]):
     def __init__(self):
@@ -78,6 +100,18 @@ class Grid(Generic[T]):
         """Return an iterator over all points for which `f` returns True."""
         return (p for p in self.points() if f(p, self.grid[p.y][p.x]))
 
+    def find_item(self, item: T) -> Iterator[Point]:
+        """Return an iterator over all points at which an item equal to `item` is stored."""
+        return (p for p in self.points() if item == self.grid[p.y][p.x])
+
+    def find_any(self, f: Callable[[Point, T], bool]) -> bool:
+        """Return whether there's an item for which `f` returns True."""
+        return any(f(p, self.grid[p.y][p.x]) for p in self.points())
+
+    def find_first(self, f: Callable[[Point, T], bool]) -> Optional[Point]:
+        """Return the position of the first item for which `f` returns True (None if nothing matched)."""
+        return next((p for p in self.points() if f(p, self.grid[p.y][p.x])), None)
+
     def row(self, y: int) -> Iterator[T]:
         """Return an iterator over all elements in the row with the given index `y`."""
         return iter(self.grid[y])
@@ -95,6 +129,26 @@ class Grid(Generic[T]):
         """Apply `f` to each element. `f` is a function `Point, val -> new_val`"""
         for p in self.points():
             self.grid[p.y][p.x] = f(p, self.grid[p.y][p.x])
+
+    def replace(self, old_item: T, new_item: T):
+        """Replace every item equal to `old_item` by `new_item`."""
+        for p in self.find_item(old_item):
+            self.grid[p.y][p.x] = new_item
+
+    def find_replace(self, find_item: Callable[[Point, T], bool], new_item: T):
+        """Replace every item for which `find_item` returns True by `new_item`."""
+        for p in self.find(find_item):
+            self.grid[p.y][p.x] = new_item
+
+    def replace_with_init(self, old_item: T, init_new_item: Callable[[Point], T]):
+        """Replace every item equal to `old_item` by a new item initialized by `init_new_item`."""
+        for p in self.find_item(old_item):
+            self.grid[p.y][p.x] = init_new_item(p)
+
+    def find_replace_with_init(self, find_item: Callable[[Point, T], bool], init_new_item: Callable[[Point, T], T]):
+        """Replace every item for which `find_item` returns True by a new item initialized by `init_new_item`."""
+        for p in self.find(find_item):
+            self.grid[p.y][p.x] = init_new_item(p)
 
     def points(self) -> Iterator[Point]:
         """
