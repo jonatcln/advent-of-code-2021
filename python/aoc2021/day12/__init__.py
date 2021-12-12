6356ec907f303a790073a06ad628de6dfbe38443
jonatcln <41806047+jonatcln@users.noreply.github.com>
2021-12-12 12:25:20 +0100

    refactor(py/d12): improve performance

--- python/aoc2021/day12/__init__.py
+++ python/aoc2021/day12/__init__.py
@@ -4,37 +4,43 @@ from collections import defaultdict
 
 
 @aoc.solver(day=12)
-def part1(data: str):
+def run(data: str):
     graph = defaultdict(set)
     for line in data.splitlines():
         p = line.split("-")
         graph[p[0]].add(p[1])
         graph[p[1]].add(p[0])
     return (
-        sum(1 for _ in find_paths1(graph, ["start"])),
-        sum(1 for _ in find_paths2(graph, ["start"])),
+        find_paths1(graph, set(), "start"),
+        find_paths2(graph, set(), "start"),
     )
 
 
-def find_paths1(edges, path):
-    node = path[-1]
-    for n in edges[node]:
-        if n == "end":
-            yield [*path, n]
-        elif n.isupper() or n not in path:
-            yield from find_paths1(edges, [*path, n])
-
-
-def find_paths2(edges, path, twice=False):
-    node = path[-1]
+def find_paths1(edges, lowers, node):
+    count = 0
     for n in edges[node]:
         if n == "start":
             continue
         elif n == "end":
-            yield [*path, n]
+            count += 1
         elif n.isupper():
-            yield from find_paths2(edges, [*path, n], twice)
+            count += find_paths1(edges, lowers, n)
+        elif n not in lowers:
+            count += find_paths1(edges, lowers | {n}, n)
+    return count
+
+
+def find_paths2(edges, lowers, node, twice=False):
+    count = 0
+    for n in edges[node]:
+        if n == "start":
+            continue
+        elif n == "end":
+            count += 1
+        elif n.isupper():
+            count += find_paths2(edges, lowers, n, twice)
         elif not twice:
-            yield from find_paths2(edges, [*path, n], n in path)
-        elif n not in path:
-            yield from find_paths2(edges, [*path, n], twice)
+            count += find_paths2(edges, lowers | {n}, n, n in lowers)
+        elif n not in lowers:
+            count += find_paths2(edges, lowers | {n}, n, twice)
+    return count
